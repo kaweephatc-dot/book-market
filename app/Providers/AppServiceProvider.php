ac4bc442b49625b737e0aa9e1789ab180e9f699e
kaweephat <kaweephat.c@nsru.ac.th>
2026-07-03 00:40:03 +0700

    เตรียมไฟล์สำหรับ deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // บังคับ HTTPS เมื่ออยู่บน production (server จริง)
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
